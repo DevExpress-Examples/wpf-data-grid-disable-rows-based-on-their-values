50be027f081feb08ba50e271bb1bd4856f1b9e83
DevExpressExampleBot <fakeEmail>
2021-10-27 18:50:48 +0300

    autogenerate VB

---
+++ VB/WpfApplication/App.xaml.vb
@@ -0,0 +1,9 @@
+Imports System.Windows
+
+Namespace WpfApplication
+
+    Public Partial Class App
+        Inherits Application
+
+    End Class
+End Namespace
--- VB/WpfApplication/MainWindow.xaml.vb
+++ VB/WpfApplication/MainWindow.xaml.vb
@@ -1,19 +1,12 @@
-﻿Imports System.Windows
-Imports DevExpress.Xpf.Grid
+Imports System.Windows
 
 Namespace WpfApplication
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub tableView1_ShowingEditor(ByVal sender As Object, ByVal e As ShowingEditorEventArgs)
-			e.Cancel = Not (TryCast(gridControl1.CurrentItem, GridItem)).AllowEdit
-		End Sub
-	End Class
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
 End Namespace
