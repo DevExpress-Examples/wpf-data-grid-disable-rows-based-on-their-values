c33d2fd6f76b9ffeb6963ecc917c10984eea59d2
DevExpressExampleBot <fakeEmail>
2021-11-16 12:00:28 +0300

    Source auto update [skip ci]

--- VB/WpfApplication/MainWindow.xaml.vb
+++ VB/WpfApplication/MainWindow.xaml.vb
@@ -1,19 +1,20 @@
-﻿Imports System.Windows
+Imports System.Windows
 Imports DevExpress.Xpf.Grid
 
 Namespace WpfApplication
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub tableView1_ShowingEditor(ByVal sender As Object, ByVal e As ShowingEditorEventArgs)
-			e.Cancel = Not (TryCast(gridControl1.CurrentItem, GridItem)).AllowEdit
-		End Sub
-	End Class
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub tableView1_ShowingEditor(ByVal sender As Object, ByVal e As ShowingEditorEventArgs)
+            e.Cancel = Not TryCast(gridControl1.CurrentItem, GridItem).AllowEdit
+        End Sub
+    End Class
 End Namespace
