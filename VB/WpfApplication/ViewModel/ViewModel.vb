dc37bb371e0d04ac2f3d45fc5c106285d6bffff4
DevExpressExampleBot <fakeEmail>
2021-11-16 11:48:23 +0300

    autogenerate VB

--- VB/WpfApplication/ViewModel/ViewModel.vb
+++ VB/WpfApplication/ViewModel/ViewModel.vb
@@ -1,30 +1,22 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-
 Namespace WpfApplication
-	Public Class ViewModel
 
-		Public Sub New()
+    Public Class ViewModel
 
-		End Sub
+        Public Sub New()
+        End Sub
 
-		' Fields...
-		Private _DataSource As Object
+        ' Fields...
+        Private _DataSource As Object
 
+        Public Property DataSource As Object
+            Get
+                If _DataSource Is Nothing Then _DataSource = DataHelper.GetDataSource(20)
+                Return _DataSource
+            End Get
 
-		Public Property DataSource() As Object
-			Get
-				If _DataSource Is Nothing Then
-					_DataSource = DataHelper.GetDataSource(20)
-				End If
-				Return _DataSource
-			End Get
-			Set(ByVal value As Object)
-				_DataSource = value
-
-			End Set
-		End Property
-	End Class
+            Set(ByVal value As Object)
+                _DataSource = value
+            End Set
+        End Property
+    End Class
 End Namespace
