2ed08dd8529923467a6e7b26eb6d3d0edb731457
DevExpressExampleBot <fakeEmail>
2026-01-30 07:06:39 +0400

    Source auto update [skip ci]

--- VB/WpfApplication/RowStateHelper/RowStateConverter.vb
+++ VB/WpfApplication/RowStateHelper/RowStateConverter.vb
@@ -1,40 +1,31 @@
-Imports Microsoft.VisualBasic
 Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports System.Windows.Data
 Imports System.Windows.Markup
 Imports System.Windows.Media
 
 Namespace WpfApplication
-	Public Class RowStateConverter
-		Inherits MarkupExtension
-		Implements IValueConverter
-		Public Sub New()
 
-		End Sub
+    Public Class RowStateConverter
+        Inherits MarkupExtension
+        Implements IValueConverter
 
+        Public Sub New()
+        End Sub
 
-		Private Function IValueConverter_Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.Convert
-			Dim state As Boolean =CBool(value)
-			If Object.Equals(parameter, "Background") Then
-				Return New SolidColorBrush(If(state, Colors.White, Colors.LightGray))
-			End If
-			If Object.Equals(parameter, "Foreground") Then
-				Return New SolidColorBrush(If(state, Colors.Black, Colors.DarkGray))
-			End If
-			If Object.Equals(parameter, "IsEnabled") Then
-				Return state
-			End If
-			Return Nothing
-		End Function
+        Private Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As Globalization.CultureInfo) As Object Implements IValueConverter.Convert
+            Dim state As Boolean = CBool(value)
+            If Equals(parameter, "Background") Then Return New SolidColorBrush(If(state, Colors.White, Colors.LightGray))
+            If Equals(parameter, "Foreground") Then Return New SolidColorBrush(If(state, Colors.Black, Colors.DarkGray))
+            If Equals(parameter, "IsEnabled") Then Return state
+            Return Nothing
+        End Function
 
-		Private Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
-			Return Nothing
-		End Function
-		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
-			Return Me
-		End Function
+        Private Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As Globalization.CultureInfo) As Object Implements IValueConverter.ConvertBack
+            Return Nothing
+        End Function
 
-	End Class
+        Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+            Return Me
+        End Function
+    End Class
 End Namespace
--- VB/WpfApplication/RowStateHelper/RowStateHelper.vb
+++ VB/WpfApplication/RowStateHelper/RowStateHelper.vb
@@ -1,46 +1,38 @@
-Imports Microsoft.VisualBasic
-Imports System
-Imports System.Collections.Generic
-Imports System.Linq
 Imports System.Windows
 Imports DevExpress.Xpf.Grid
-Imports System.Windows.Data
 
 Namespace WpfApplication
-	Public NotInheritable Class RowStateHelper
-		Public Shared ReadOnly IsHelperEnabledProperty As DependencyProperty = DependencyProperty.RegisterAttached("IsHelperEnabled", GetType(Boolean), GetType(RowStateHelper), New UIPropertyMetadata(False, New PropertyChangedCallback(AddressOf OnIsHelperEnabledChanged)))
 
-		Private Sub New()
-		End Sub
-		Public Shared Function GetIsHelperEnabled(ByVal target As DependencyObject) As Boolean
-			Return CBool(target.GetValue(IsHelperEnabledProperty))
-		End Function
-		Public Shared Sub SetIsHelperEnabled(ByVal target As DependencyObject, ByVal value As Boolean)
-			target.SetValue(IsHelperEnabledProperty, value)
-		End Sub
-		Private Shared Sub OnIsHelperEnabledChanged(ByVal o As DependencyObject, ByVal e As DependencyPropertyChangedEventArgs)
-			OnIsHelperEnabledChanged(o, CBool(e.OldValue), CBool(e.NewValue))
-		End Sub
-		Private Shared Sub OnIsHelperEnabledChanged(ByVal o As DependencyObject, ByVal oldValue As Boolean, ByVal newValue As Boolean)
-			Dim view As GridViewBase = TryCast(o, GridViewBase)
-			RemoveHandler view.ShowingEditor, AddressOf RowStateHelper_ShowingEditor
-			If newValue Then
-				AddHandler view.ShowingEditor, AddressOf RowStateHelper_ShowingEditor
-			End If
+    Public Module RowStateHelper
 
-		End Sub
+        Public ReadOnly IsHelperEnabledProperty As DependencyProperty = DependencyProperty.RegisterAttached("IsHelperEnabled", GetType(Boolean), GetType(RowStateHelper), New UIPropertyMetadata(False, New PropertyChangedCallback(AddressOf OnIsHelperEnabledChanged)))
 
-		Private Shared Sub RowStateHelper_ShowingEditor(ByVal sender As Object, ByVal e As ShowingEditorEventArgs)
-			e.Cancel = Not IsRowEnabled(e.Row)
-		End Sub
+        Public Function GetIsHelperEnabled(ByVal target As DependencyObject) As Boolean
+            Return CBool(target.GetValue(IsHelperEnabledProperty))
+        End Function
 
-		Public Shared Function IsRowEnabled(ByVal row As Object) As Boolean
-			Dim datarow As GridItem = TryCast(row, GridItem)
-			If datarow Is Nothing Then
-				Return True
-			End If
-			Return datarow.AllowEdit
-		End Function
+        Public Sub SetIsHelperEnabled(ByVal target As DependencyObject, ByVal value As Boolean)
+            target.SetValue(IsHelperEnabledProperty, value)
+        End Sub
 
-	End Class
+        Private Sub OnIsHelperEnabledChanged(ByVal o As DependencyObject, ByVal e As DependencyPropertyChangedEventArgs)
+            Call OnIsHelperEnabledChanged(o, CBool(e.OldValue), CBool(e.NewValue))
+        End Sub
+
+        Private Sub OnIsHelperEnabledChanged(ByVal o As DependencyObject, ByVal oldValue As Boolean, ByVal newValue As Boolean)
+            Dim view As GridViewBase = TryCast(o, GridViewBase)
+            RemoveHandler view.ShowingEditor, AddressOf RowStateHelper_ShowingEditor
+            If newValue Then AddHandler view.ShowingEditor, AddressOf RowStateHelper_ShowingEditor
+        End Sub
+
+        Private Sub RowStateHelper_ShowingEditor(ByVal sender As Object, ByVal e As ShowingEditorEventArgs)
+            e.Cancel = Not IsRowEnabled(e.Row)
+        End Sub
+
+        Public Function IsRowEnabled(ByVal row As Object) As Boolean
+            Dim datarow As GridItem = TryCast(row, GridItem)
+            If datarow Is Nothing Then Return True
+            Return datarow.AllowEdit
+        End Function
+    End Module
 End Namespace
